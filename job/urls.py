8d60647523ded3a6f0bd60372c6f9c89b1c5786d
PhamMinhDuc <hotro.army@gmail.com>
2019-05-30 18:38:06 +0700

    duc
    them code

--- job/urls.py
+++ job/urls.py
@@ -1,16 +1,23 @@
+from django.conf.urls import url
+from django.conf.urls.static import static
 from django.urls import path
 from job import views
-from job.views import ViecLamDetailView
+from job.views import ViecLamDetailView, Viecfreelances
+from django.conf import settings
+
 
 app_name = 'job'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('dang-du-an.html', views.Postlist.as_view(), name='list'),
-    path('viec-lam-freelance.html', ViecLamDetailView.as_view(), name='lisst tesst'),
-    path('index.html', views.index, name='homepage'),
-    path('dang-du-an.html', views.dangduan, name='dang-du-an'),
-    path('dang-viec-tuyen-dung.html', views.dangviectuyendung, name='dang-viec-tuyen-dung'),
-    path('dang-cuoc-thi.html', views.dangcuocthi, name='dang-cuoc-thi'),
-    path('viec-lam-freelance.html', views.vieclamfreelance, name='viec-lam'),
-]
+                  path('', views.index, name='index'),
+                  path('dang-du-an.html', views.Postlist.as_view(), name='list'),
+                  path('viec-lam-freelance.html', ViecLamDetailView.as_view(), name='lisst tesst'),
+                  path('index.html', views.index, name='homepage'),
+                  path('dang-du-an.html', views.dangduan, name='dang-du-an'),
+                  path('dang-viec-tuyen-dung.html', views.dangviectuyendung, name='dang-viec-tuyen-dung'),
+                  path('dang-cuoc-thi.html', views.dangcuocthi, name='dang-cuoc-thi'),
+                  path('viec-lam-freelance.html', views.vieclamfreelance, name='viec-lam'),
+
+                  path('viec-freelance/<str:slug>/', views.Viecfreelances, name='product'),
+
+              ] + static(settings.STATIC_URL, document_root=settings.MEDIA_ROOT)
 
--- job/views.py
+++ job/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 from .models import DichVu, LinhVuc, ThanhPho, ViecTheoDuAn, KyNang
 from django.views.generic import ListView , DetailView , CreateView ,UpdateView ,DeleteView
@@ -41,8 +42,7 @@ class Postlist(ListView):
         context['post1'] = LinhVuc.objects.all()
         return context
 
-
-#DetailView
+#Lisst viec lam freelance
 
 class ViecLamDetailView(ListView):
     template_name = 'viec-lam-freelance.html'
@@ -58,3 +58,17 @@ class ViecLamDetailView(ListView):
 
         return context
 
+
+class ViecLamDetailView(ListView):
+    template_name = 'viec-lam-freelance.html'
+    model = ViecTheoDuAn
+    context_object_name = 'VL'
+    queryset = ViecTheoDuAn.objects.all()
+
+
+def Viecfreelances(request, slug):
+    try:
+        VC = ViecTheoDuAn.objects.get(slug=slug)
+    except ViecTheoDuAn.DoesNotExist:
+        raise Http404("Loi bai viet roi !! Lien he DUC ngay !! ")
+    return render(request, 'viec-freelance/index.html', {'VC': VC})
